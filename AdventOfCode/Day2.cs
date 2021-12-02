400d1721d885df11e59b9ae5b4471e98d334154b
Rasmus Bjørneskov <rb@siteshop.dk>
2021-12-02 14:23:22 +0100

    Setup for Day 2 as well as made a template for the days

---
+++ AdventOfCode/Day2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day2
+    {
+        Utilities utilities = new Utilities();
+        public Day2()
+        {
+        }
+        public void Challenge1()
+        {
+
+        }
+        public void Challenge2()
+        {
+
+        }
+    }
+}
---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day3
+    {
+        Utilities utilities = new Utilities();
+        public Day3()
+        {
+        }
+        public void Challenge1()
+        {
+
+        }
+        public void Challenge2()
+        {
+
+        }
+    }
+}
