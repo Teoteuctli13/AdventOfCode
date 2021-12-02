955eb8d959fda29c6e8bc74aec4a7daaf14e2538
Rasmus Bjørneskov <rb@siteshop.dk>
2021-12-02 13:44:24 +0100

    Add project files.

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        Utilities utilities = new Utilities();
+        public Day1()
+        {
+        }
+
+        public void Challenge1()
+        {
+            int increases = 0;
+            string content = utilities.ReadFile("Day1.txt");
+            List<int> deapthMeassures = content.Split("\n").Select(Int32.Parse).ToList();
+            for (int i = 1; i < deapthMeassures.Count; i++)
+            {
+
+                if (deapthMeassures[i] > deapthMeassures[i - 1])
+                {
+                    increases++;
+                }
+            }
+            Console.WriteLine(increases);
+            Console.ReadLine();
+        }
+        public void Challenge2()
+        {
+            int increases = 0;
+            string content = utilities.ReadFile("Day1.txt");
+            List<int> deapthMeassures = content.Split("\n").Select(Int32.Parse).ToList();
+            for (int i = 1; i < deapthMeassures.Count-2; i++)
+            {
+                int a = deapthMeassures[i - 1] + deapthMeassures[i] + deapthMeassures[i + 1];
+                int b = deapthMeassures[i] + deapthMeassures[i + 1] + deapthMeassures[i + 2];
+
+                if (a<b)
+                {
+                    increases++;
+                }
+            }
+            Console.WriteLine(increases);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Utilities
+    {
+        private string Folder = @"..\..\..\Input\";
+        public Utilities()
+        {        
+        }
+        public string ReadFile(string inputFile)
+        {
+            using (StreamReader streamReader = new StreamReader(Folder+inputFile))
+            {
+                string fileContent = streamReader.ReadToEnd();
+                return fileContent;
+            }
+        }
+
+    }
+}
